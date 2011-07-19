cede3b85f645000462a179b4a6a2bbef299f6895
Garry Dolley <gdolley@arpnetworks.com>
2011-07-19 01:28:48 -0700

    Refactored Credit Card model
    Credit card security standards (e.g. PCI Data Security Standard)
    prohibit storing the CVV, so I've removed it.
    
    The credit card number field has been converted to a variable length
    text field, to hold the encrypted version of the number.

---
+++ db/migrate/20110719073345_refactor_credit_cards.rb
@@ -0,0 +1,11 @@
+class RefactorCreditCards < ActiveRecord::Migration
+  def self.up
+    remove_column :credit_cards, :number
+    remove_column :credit_cards, :cvv
+
+    add_column :credit_cards, :number, :text
+  end
+
+  def self.down
+  end
+end
