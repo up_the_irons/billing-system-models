5367b82e0db9aa87308bce6a06f5a557b1ab5b0c
Garry Dolley <gdolley@arpnetworks.com>
2012-01-17 03:30:05 -0800

    SalesReceipt line items are also dependent

--- lib/models/sales_receipt.rb
+++ lib/models/sales_receipt.rb
@@ -1,6 +1,6 @@
 class SalesReceipt < ActiveRecord::Base
   belongs_to :account
-  has_many :line_items, :class_name => 'SalesReceiptsLineItem'
+  has_many :line_items, :class_name => 'SalesReceiptsLineItem', :dependent => :destroy
 
   class LineItemSumError < StandardError; end
 end
