14a3bc47ebb93d50475bee6a5b9a5c6ec99b45fb
Garry Dolley <gdolley@arpnetworks.com>
2012-02-01 04:19:02 -0800

    Invoice line item date defaults to invoice date, when not provided

--- lib/models/invoices_line_item.rb
+++ lib/models/invoices_line_item.rb
@@ -1,3 +1,12 @@
 class InvoicesLineItem < ActiveRecord::Base
   belongs_to :invoice
+
+  before_validation :assign_date
+  validates_presence_of :date
+
+  def assign_date
+    if date.nil?
+      self.date = invoice.date
+    end
+  end
 end
