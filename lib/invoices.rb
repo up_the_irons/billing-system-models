80472ca69f3b2c84c3a3647e4100a1674e231ecc
Garry Dolley <gdolley@arpnetworks.com>
2013-11-19 02:41:51 -0800

    Practice Law of Demeter with some methods on invoices

--- lib/invoices.rb
+++ lib/invoices.rb
@@ -17,6 +17,24 @@ module BillingSystemModels
           nil
         end
       end
+
+      def invoices_paid
+        invoices.paid
+      end
+
+      def invoices_unpaid
+        invoices.unpaid
+      end
+
+      def invoices_outstanding_balance
+        if invoices_unpaid.nil?
+          return 0
+        end
+
+        invoices_unpaid.inject(0) do |sum, invoice|
+          invoice.balance + sum
+        end
+      end
     end
   end
 end
---
+++ spec/invoices_spec.rb
@@ -0,0 +1,71 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+class Foo
+  def self.has_many(foo)
+    # Stub
+  end
+
+  include BillingSystemModels::Invoices
+end
+
+describe 'Invoices' do
+  describe 'InstanceMethods' do
+    before do
+      @my_class = Foo.new
+    end
+
+    describe 'invoices_paid()' do
+      it 'should call invoices.paid' do
+        @invoices = double(:invoices)
+        @invoices.should_receive(:paid)
+        @my_class.should_receive(:invoices).and_return(@invoices)
+        @my_class.invoices_paid
+      end
+    end
+
+    describe 'invoices_unpaid()' do
+      it 'should call invoices.unpaid' do
+        @invoices = double(:invoices)
+        @invoices.should_receive(:unpaid)
+        @my_class.should_receive(:invoices).and_return(@invoices)
+        @my_class.invoices_unpaid
+      end
+    end
+
+    describe 'invoices_outstanding_balance' do
+      describe 'when no invoices' do
+        before do
+          @my_class.stub!(:invoices_unpaid).and_return(nil)
+        end
+
+        it 'should return zero' do
+          @my_class.invoices_outstanding_balance.should == 0
+        end
+      end
+
+      describe 'when all invoices are paid' do
+        before do
+          @my_class.stub!(:invoices_unpaid).and_return([])
+        end
+
+        it 'should return zero' do
+          @my_class.invoices_outstanding_balance.should == 0
+        end
+      end
+
+      describe 'when some invoices are unpaid' do
+        before do
+          @unpaid_invoices = [
+            double(:invoice, :balance => 20.00),
+            double(:invoice, :balance => 1.00)
+          ]
+          @my_class.stub!(:invoices_unpaid).and_return(@unpaid_invoices)
+        end
+
+        it 'should return total unpaid balance' do
+          @my_class.invoices_outstanding_balance.should == 21.00
+        end
+      end
+    end
+  end
+end
